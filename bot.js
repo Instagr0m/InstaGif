65efb7acdf46a43953a0cd88a99f5a88201f0aee
Instagr0m <84603000+Instagr0m@users.noreply.github.com>
2021-05-22 01:20:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.login(process.env.DISCORD_BOT_TOKEN);
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
